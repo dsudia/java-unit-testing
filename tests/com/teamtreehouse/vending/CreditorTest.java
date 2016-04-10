c04076910e808332919ce3a9a2de154639152122
David Sudia <dsudia@gmail.com>
2016-04-10 17:12:28 -0600

    added test for refund method

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -9,8 +9,23 @@ import static org.junit.Assert.*;
  */
 public class CreditorTest {
     @Test
-    public void refund() throws Exception {
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
 
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        assertEquals(50, creditor.getAvailableFunds());
     }
 
+    @Test
+    public void refundingReturnsAllAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+        creditor.addFunds(10);
+
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
+        assert
+    }
 }
