6fdb14ffe3237ff00c0ff089d383e53f17203a16
David Sudia <dsudia@gmail.com>
2016-04-09 18:12:02 -0600

    created testing file

---
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,16 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidsudia on 4/9/16.
+ */
+public class CreditorTest {
+    @Test
+    public void refund() throws Exception {
+
+    }
+
+}
